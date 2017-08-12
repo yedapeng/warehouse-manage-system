2cd3095ef5559e7471760e82484a822862267e0a
depen home pc <chinayedapeng@gmail.com>
2017-08-12 23:05:06 +0800

    创建后首次提交

---
+++ code project/main.c
@@ -0,0 +1,53 @@
+/*******************************************************************************
+
+
+
+作者：叶大鹏
+创建时间：2017.0812
+
+
+
+*******************************************************************************/
+
+
+
+
+
+
+
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm8s.h"
+
+
+
+int main( void )
+{
+  return 0;
+}
+
+
+
+
+
+
+#ifdef USE_FULL_ASSERT
+
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *   where the assert_param error has occurred.
+  * @param file: pointer to the source file name
+  * @param line: assert_param error line source number
+  * @retval : None
+  */
+void assert_failed(u8* file, u32 line)
+{ 
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+
+  /* Infinite loop */
+  while (1)
+  {
+  }
+}
+#endif
